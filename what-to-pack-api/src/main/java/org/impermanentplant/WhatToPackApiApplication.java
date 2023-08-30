ecc7073bc0b73204643310f64bbbb59713d53f71
EricT1900 <farAwayKnight182@gmail.com>
2023-08-29 22:29:43 -0700

    Added comment for application

--- what-to-pack-api/src/main/java/org/impermanentplant/WhatToPackApiApplication.java
+++ what-to-pack-api/src/main/java/org/impermanentplant/WhatToPackApiApplication.java
@@ -3,6 +3,9 @@ package org.impermanentplant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * API for what-to-pack project
+ */
 @SpringBootApplication
 public class WhatToPackApiApplication {
     public static void main(String[] args) {
