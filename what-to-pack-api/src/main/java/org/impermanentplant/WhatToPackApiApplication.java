c3a9a913802752d4a292c51121a0e0937fe3234f
EricT1900 <farAwayKnight182@gmail.com>
2023-08-29 22:20:20 -0700

    Setup project with spring boot and Java 17

---
+++ what-to-pack-api/src/main/java/org/impermanentplant/WhatToPackApiApplication.java
@@ -0,0 +1,11 @@
+package org.impermanentplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToPackApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WhatToPackApiApplication.class, args);
+    }
+}
