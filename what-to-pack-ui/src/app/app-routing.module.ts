1408711f753bf1ef5675da48e6ac76483583def4
JonathanZurita <jonrzurita@gmail.com>
2023-09-01 14:17:13 -0700

    adds landing page and routes

--- what-to-pack-ui/src/app/app-routing.module.ts
+++ what-to-pack-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'landingPage', component: LandingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
