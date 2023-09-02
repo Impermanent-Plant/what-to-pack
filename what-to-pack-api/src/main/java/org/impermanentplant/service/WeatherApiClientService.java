38c1b0028d2aece3a1417e1573da13db605b9c55
EricT1900 <farAwayKnight182@gmail.com>
2023-09-01 18:58:52 -0700

    Create and implement service pulling data from weather API

---
+++ what-to-pack-api/src/main/java/org/impermanentplant/service/WeatherApiClientService.java
@@ -0,0 +1,9 @@
+package org.impermanentplant.service;
+
+import org.impermanentplant.model.WeatherApiDaily;
+
+/**
+ * Service to retrieve Weather Data from external API
+ */
+public interface WeatherApiClientService {
+}
---
+++ what-to-pack-api/src/main/java/org/impermanentplant/service/impl/WeatherApiClientServiceImpl.java
@@ -0,0 +1,41 @@
+package org.impermanentplant.service.impl;
+
+import org.impermanentplant.model.WeatherApiDaily;
+import org.impermanentplant.model.WeatherApiResponse;
+import org.impermanentplant.service.WeatherApiClientService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+/**
+ * Implementation of WeatherApiClientService to retrieve weather data from external API
+ */
+@Service
+public class WeatherApiClientServiceImpl implements WeatherApiClientService {
+
+    private final WebClient webClient;
+    private final static String WEATHER_ENDPOINT_URI = "v1/archive?latitude={latitude}&longitude={longitude}&" +
+            "start_date={startDate}&end_date={endDate}&daily=temperature_2m_max,temperature_2m_min," +
+            "temperature_2m_mean&timezone=America/New_York";
+
+    public WeatherApiClientServiceImpl(WebClient.Builder webClientBuilder,
+                                       @Value("${weather-api.url}") String baseUrl) {
+        this.webClient = webClientBuilder.baseUrl(baseUrl).build();
+    }
+
+    /**
+     * Retrieve weather data from API
+     * @param latitude Latitude of location
+     * @param longitude Longitude of location
+     * @param startDate Start date for weather
+     * @param endDate End date for weather
+     * @return An object containing weather data for each day from startDate to endDate
+     */
+    private WeatherApiDaily retrieveDailyWeather(double latitude, double longitude, String startDate, String endDate) {
+        Mono<WeatherApiResponse> weatherApiResponseMono = webClient.get().
+                uri(WEATHER_ENDPOINT_URI, latitude, longitude, startDate, endDate).
+                retrieve().bodyToMono(WeatherApiResponse.class);
+        return weatherApiResponseMono.block().getDaily();
+    }
+}
