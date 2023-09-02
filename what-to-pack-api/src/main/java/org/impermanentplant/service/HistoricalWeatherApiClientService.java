b11f53192350ab59c65e0918bbcd67b65fc6e1ae
EricT1900 <farAwayKnight182@gmail.com>
2023-09-01 19:35:53 -0700

    Rename weather-api to historical-weather-api to be more accurate

--- what-to-pack-api/src/main/java/org/impermanentplant/service/HistoricalWeatherApiClientService.java
+++ what-to-pack-api/src/main/java/org/impermanentplant/service/HistoricalWeatherApiClientService.java
@@ -1,9 +1,7 @@
 package org.impermanentplant.service;
 
-import org.impermanentplant.model.WeatherApiDaily;
-
 /**
  * Service to retrieve Weather Data from external API
  */
-public interface WeatherApiClientService {
+public interface HistoricalWeatherApiClientService {
 }
--- what-to-pack-api/src/main/java/org/impermanentplant/service/impl/HistoricalWeatherApiClientServiceImpl.java
+++ what-to-pack-api/src/main/java/org/impermanentplant/service/impl/HistoricalWeatherApiClientServiceImpl.java
@@ -2,7 +2,7 @@ package org.impermanentplant.service.impl;
 
 import org.impermanentplant.model.WeatherApiDaily;
 import org.impermanentplant.model.WeatherApiResponse;
-import org.impermanentplant.service.WeatherApiClientService;
+import org.impermanentplant.service.HistoricalWeatherApiClientService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,15 +12,15 @@ import reactor.core.publisher.Mono;
  * Implementation of WeatherApiClientService to retrieve weather data from external API
  */
 @Service
-public class WeatherApiClientServiceImpl implements WeatherApiClientService {
+public class HistoricalWeatherApiClientServiceImpl implements HistoricalWeatherApiClientService {
 
     private final WebClient webClient;
     private final static String WEATHER_ENDPOINT_URI = "v1/archive?latitude={latitude}&longitude={longitude}&" +
             "start_date={startDate}&end_date={endDate}&daily=temperature_2m_max,temperature_2m_min," +
             "temperature_2m_mean&timezone=America/New_York";
 
-    public WeatherApiClientServiceImpl(WebClient.Builder webClientBuilder,
-                                       @Value("${weather-api.url}") String baseUrl) {
+    public HistoricalWeatherApiClientServiceImpl(WebClient.Builder webClientBuilder,
+                                                 @Value("${historical-weather-api.url}") String baseUrl) {
         this.webClient = webClientBuilder.baseUrl(baseUrl).build();
     }
 
